9e652b91d3807340baea0449129f3f07c7ac59dc
JeahaOh <jeaha1217@gmail.com>
2023-06-29 21:52:55 +0900

    2023 06 29 Interceptor & Logback

---
+++ study/vol1/spring_study/src/main/java/com/example/spring_study/mvc/common/interceptor/LoggingInterceptor.java
@@ -0,0 +1,27 @@
+package com.example.spring_study.mvc.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Created by jeaha on 2023/06/24
+ */
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+    Logger logger = LoggerFactory.getLogger(getClass());
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("preHandle requestURI : {}", request.getRequestURI());
+        return true;
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        logger.info("postHandle requestURI : {}", request.getRequestURI());
+    }
+}
--- study/vol1/spring_study/src/main/java/com/example/spring_study/mvc/config/WebConfiguration.java
+++ study/vol1/spring_study/src/main/java/com/example/spring_study/mvc/config/WebConfiguration.java
@@ -1,8 +1,11 @@
 package com.example.spring_study.mvc.config;
 
+import com.example.spring_study.mvc.common.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Locale;
 
@@ -10,7 +13,7 @@ import java.util.Locale;
  * Created by jeaha on 2023/06/27
  */
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
     
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
@@ -21,5 +24,15 @@ public class WebConfiguration {
         source.setDefaultLocale(Locale.KOREAN);
         source.setUseCodeAsDefaultMessage(true);
         return source;
+    }
+    
+    @Bean
+    public LoggingInterceptor loggingInterceptor() {
+        return new LoggingInterceptor();
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor((loggingInterceptor()));
     }
 }
