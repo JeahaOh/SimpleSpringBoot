a1b4e70a43a11092ac88950976dcf311444ed56d
JeahaOh <jeaha1217@gmail.com>
2023-07-01 21:35:07 +0900

    2023 07 01
    게시물 타입을 배열로 목록 검색기능 개선

--- study/vol1/spring_study/src/main/java/com/example/spring_study/mvc/domain/dto/BoardSearchRequest.java
+++ study/vol1/spring_study/src/main/java/com/example/spring_study/mvc/domain/dto/BoardSearchRequest.java
@@ -1,6 +1,9 @@
 package com.example.spring_study.mvc.domain.dto;
 
+import com.example.spring_study.mvc.domain.type.BoardType;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * 게시물 검색
@@ -9,4 +12,5 @@ import lombok.Data;
 @Data
 public class BoardSearchRequest {
     private String keyword;
+    private List<BoardType> boardTypes;
 }
